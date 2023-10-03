c2b0edcee75fe15fdf088eee6b1e809a25581dbb
Alexander van Saase <avsaase@gmail.com>
2023-10-03 22:03:18 +0200

    Fix examples

--- examples/game_of_life/src/main.rs
+++ examples/game_of_life/src/main.rs
@@ -190,7 +190,8 @@ fn view_controls<'a>(
             .size(16)
             .spacing(5)
             .text_size(16),
-        pick_list(preset::ALL, Some(preset), Message::PresetPicked)
+        pick_list(preset::ALL, Some(preset))
+            .on_selected(Message::PresetPicked)
             .padding(8)
             .text_size(16),
         button("Clear")
